2d437335be54dc5b5101195e83d0ecf983d935b3
AngeloDagostino <48442293+AngeloDagostino@users.noreply.github.com>
2019-05-07 18:30:03 +0200

    prova

--- 1.1/src/testBook.java
+++ 1.1/src/testBook.java
@@ -1,9 +1,9 @@
 
 public class testBook {
 public static void main(String[] args) {
-	Author ah2 = new Author ("Mat","nonsogiocare@noob.it",'m');
-	Book libro = new Book ("C'era una volta il 3° hokage, ora non piu", 69, ah2, 3);
+	Author ah1 = new Author ("Mat","nonsogiocare@noob.it",'m');
+	Book libro = new Book ("C'era una volta il 3° hokage, ora non piu", 69, ah1, 3);
 	System.out.println(libro);
-	System.out.println(ah2);
+	System.out.println(ah1);
 	}
 }
