184bb7a389e229b1424a1608d7197732172d6d86
AngeloDagostino <48442293+AngeloDagostino@users.noreply.github.com>
2019-05-07 18:33:27 +0200

    ppppp

--- 1.1/src/testBook.java
+++ 1.1/src/testBook.java
@@ -2,7 +2,7 @@
 public class testBook {
 public static void main(String[] args) {
 	Author ah1 = new Author ("Mat","nonsogiocare@noob.it",'m');
-	Book libro = new Book ("C'era una volta il 3° hokage, ora non piu", 59, ah1, 3);
+	Book libro = new Book ("C'era una volta il 3° hokage, ora non piu", 50, ah1, 3);
 	System.out.println(libro);
 	System.out.println(ah1);
 	}
