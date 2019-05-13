38745063211e8e36984dc87a18c8e30c32ca2947
MatteoFerretti98 <49650379+MatteoFerretti98@users.noreply.github.com>
2019-05-13 15:59:16 +0200

    12345

--- 1.1/src/LancioDado.java
+++ 1.1/src/LancioDado.java
@@ -3,7 +3,7 @@ public class LancioDado {
 	public static void main(String[] args) {
 	Dado d = new Dado();
 	System.out.println(d);
-	System.out.println("Sisisisisi");
+	System.out.println("Sisisisisi ");
 	d.pari();
 	d.pari();
 	}
